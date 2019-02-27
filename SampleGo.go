5ae9ecb43ab66ff125ebb241c530d065afd7cdb7
Jaime <jaime.enriquez@inodesoft.com>
2019-02-27 14:54:59 -0700

    Added Values

--- SampleGo.go
+++ SampleGo.go
@@ -11,7 +11,7 @@ func helloWorld() {
 
 func values() {
 	fmt.Println("**Example Values**")
-	fmt.Println("go" + "lang")
+	fmt.Println("go" + "lang ")
 	fmt.Println("1+1", 1+1)
 	fmt.Println("7.0/3.0 =", 7.0/3.0)
 	fmt.Println(true && true)
